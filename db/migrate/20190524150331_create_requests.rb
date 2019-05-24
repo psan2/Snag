415bbd8e68a99ee2e168f67c6d697a08442f9e28
Pat <patrick.s.santucci@gmail.com>
2019-05-24 16:30:39 +0100

    Updated CreateBeers migration

--- db/migrate/20190524150331_create_requests.rb
+++ db/migrate/20190524150331_create_requests.rb
@@ -1,8 +1,8 @@
 class CreateRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :requests do |t|
-      t.resources :requester_id
-      t.resources :snagger_id
+      t.integer :requester_id
+      t.integer :snagger_id
       t.resources :beer_id
 
       t.timestamps
