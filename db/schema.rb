254087d2efd2b6da8e4b293b450db087a7f2055e
Pat <patrick.s.santucci@gmail.com>
2019-05-28 15:57:23 +0100

    added mods table back

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_141505) do
+ActiveRecord::Schema.define(version: 2019_05_28_145417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2019_05_28_141505) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "mods", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "requests", force: :cascade do |t|
