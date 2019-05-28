025b6254e08fb008155e73631e9891d2eb7baab4
github username <github email address>
2019-05-28 10:58:55 +0100

    Update beers_controller.rb

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,3 +1,6 @@
 class BeersController < ApplicationController
-    
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :abv, presence: true
+    validates :description, presence: true 
 end
