c325309a74234cb85daf981b165ec32c1780db9e
github username <github email address>
2019-05-28 10:47:55 +0100

    added validations for beer and users
    added validations for beers and users

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,9 @@
 class Beer < ApplicationRecord
     has_many :requests
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :abv, presence: true
+    validates :description, presence: true 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_many :requests
+
+    validates :name, presence: true
+    validates :mod_id, presence: true
 end
