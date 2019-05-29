16b7d124bb571928de025388af439a903fa265b7
github username <github email address>
2019-05-29 11:22:57 +0100

    request status inclusion and in welcome page

--- app/models/request.rb
+++ app/models/request.rb
@@ -14,6 +14,7 @@ class Request < ApplicationRecord
   belongs_to :location
 
   validates_with MyValidator
+  validates :status, inclusion: { in: %w(open, closed, in progress)}
 
   def self.snags
     Request.all.select { |snag| snag.snagger_id.nil? && not(snag.requester_id.nil?) }
