6b3181d70dd58098f4019097a93ba1b26fe2bd4a
github username <github email address>
2019-05-29 10:01:04 +0100

    added locations controller and locations to requests controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,17 @@
 class LocationsController < ApplicationController
+    
+    def index 
+        @locations = Location.all 
+    end 
+
+    def show
+    end 
+
+    def new
+        @location = Location.new 
+    end 
+
+    def create 
+    end 
+
 end
