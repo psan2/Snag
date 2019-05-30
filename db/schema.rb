0d28ef4565a3a58b014b3cdfc192b4c8a817e84a
Pat <patrick.s.santucci@gmail.com>
2019-05-30 16:28:09 +0100

    relocated new page

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 2019_05_30_133529) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
     t.string "username", null: false
     t.string "crypted_password"
     t.string "salt"
