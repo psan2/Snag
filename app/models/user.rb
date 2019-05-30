2f825e8c133ee6aca9a9c7d60a78391b63ccb1b4
github username <github email address>
2019-05-30 11:55:52 +0100

    background image

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,22 +13,13 @@ class User < ApplicationRecord
   validates :password, confirmation: true
 
   def currently_snagging
-    expire_requests
     Request.find_by(snagger:self, status:"in progress")
   end
 
   def currently_requesting
-    expire_requests
     Request.find_by(requester:self, :status => ["open", "in progress"] )
   end
 
-  def expire_requests
-    Request.where(:status => ["open", "in progress"]).each do |req|
-      if (req.updated_at + 2.hours).past?
-        req.update(status: "closed")
-        req.save
-      end
-    end
-  end
+ 
 
 end
