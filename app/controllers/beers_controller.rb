3b171f7a3b8f11bab718132bc69b340f1b00b87f
Pat <patrick.s.santucci@gmail.com>
2019-06-06 12:03:32 +0100

    first branch

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,59 +1,59 @@
 class BeersController < ApplicationController
     before_action :find_beer, only: [:edit, :show, :update, :destroy]
 
-    def index 
-        @beers = Beer.all 
-    end 
+    def index
+        @beers = Beer.all
+    end
 
-    def show 
-    end 
+    def show
+    end
 
-    def new 
+    def new
         @beer = Beer.new
-    end 
+    end
 
-    def create 
+    def create
         @beer = Beer.new(beer_params)
         if @beer.valid?
             @beer.save
             flash[:notice] = "Beer was saved"
             redirect_to beers_path
-        else 
+        else
             flash[:errors] = @beer.errors.full_messages
             flash[:notice] = "Brand was not saved"
-            render :new 
+            render :new
         end
-    end 
+    end
 
-    def edit 
-    end 
+    def edit
+    end
 
-    def update 
+    def update
         @beer.update(beer_params)
         if @beer.valid?
             redirect_to @beer
             flash[:notice] = "Beer has been udpated"
-        else 
+        else
             flash[:errors] = @beer.errors.full_messages
             flash[:notice] = "Try again"
             redirect_to edit_beer_path
         end
-    end 
+    end
 
-    def destroy 
+    def destroy
         @beer.destroy
         flash[:notice] = "Beer has been deleted"
         redirect_to brands_path
-    end 
+    end
 
     private
 
-    def beer_params 
+    def beer_params
         params.require(:beer).permit(:name, :abv, :description)
-    end 
+    end
 
-    def find_beer 
+    def find_beer
         @beer = Beer.find(params[:id])
-    end 
-    
+    end
+
 end
